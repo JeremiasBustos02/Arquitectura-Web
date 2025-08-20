c4198713df29428ab30b8c5783a2255e6e579136
jeremiasbustos02 <jeremias.e.bustos@gmail.com>
2025-08-19 22:36:29 -0300

    Ejercicios TP1

--- TrabajosPracticos/TP1/JDBCBasicoMySQLDAO/src/edu/isistan/db/Main.java
+++ TrabajosPracticos/TP1/JDBCBasicoMySQLDAO/src/edu/isistan/db/Main.java
@@ -2,12 +2,18 @@ package edu.isistan.db;
 
 public class Main {
 
+	/*
+	 Las ventajas que proporciona el patrón DAO:
+	 	- Separa la responsabilidad
+	 	- Escalabilidad y mantenibilidad
+	 	- Menos acoplamiento
+	 	- Reutilización y limpieza
+	 */
+	
 	public static void main(String[] args) throws Exception {
 		PersonaDAO personaDAO = new MySQLPersonaDAO();
 		PersonaService personaService = new PersonaService(personaDAO);
-				
-		personaService.addPersona(new Persona(3, "El negro manzino", 22));
-		
+						
 		for (Persona p : personaService.getPersonas()) {
             System.out.println(p.toString());
         }
